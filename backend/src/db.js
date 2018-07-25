ff2198102147fd0b6f740642cf5bac51e974a0af
Fisso <danielepiscaglia@libero.it>
2018-07-26 00:06:43 +0200

    Modifica DB a mongos sharded cluster su unibo(localhost)

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,9 +1,9 @@
-const url = 'mongodb://admin:admin00@ds139890.mlab.com:39890/pings';
+const url = 'mongodb://localhost:27042/pings';
 // const url = 'mongodb://albertobagnacani:modeling4cloud@10.0.0.14/pings'; // TODO secrets?
 //const url = 'mongodb://albertobagnacani:modeling4cloud@137.204.57.93:27017/pings'; // TODO secrets?
 var mongoose = require('mongoose');
 
 mongoose.connect(url, function(err, db){
-    if (err) console.log('Unable to connect to DB. Error: ' + err);
-    else console.log('Connection established to DB');
+    if (err) console.log('Unable to connect to DB ' + url + '. Error: ' + err);
+    else console.log('Connection established to DB:' + url);
 });

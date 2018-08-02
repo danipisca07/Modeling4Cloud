36c0919e336759f4ef680346813ecd1d59909868
Fisso <danielepiscaglia@libero.it>
2018-08-02 18:06:58 +0200

    Aggiunto script di avvio del backend/frontend su cloud unibo

--- utils/instructions/startBackend.sh
+++ utils/instructions/startBackend.sh
@@ -3,9 +3,17 @@ ssh -t -i ~/keys/dpiscaglia.pem ubuntu@137.204.57.136 'sudo bash -i -c "
 rm -r -f Modeling4Cloud/
 git clone https://github.com/danipisca07/Modeling4Cloud.git
 
+echo "Setup backend"
 cd ~/Modeling4Cloud/backend
 
 npm install
 npm install -g nodemon
 export PORT=3100
-nohup nodemon start > foo.out 2> foo.err < /dev/null &"'
+nohup nodemon start > backend.out 2> backend.err < /dev/null &
+
+echo "Setup frontend"
+cd ~/Modeling4Cloud/frontend
+
+sudo npm install
+nohup npm run-script start-dev > frontend.out 2> frontend.err < /dev/null &
+"'

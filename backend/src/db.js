7522d574a3032e415c5e1c9c846f16f05606af6a
Fisso <danielepiscaglia@libero.it>
2018-08-03 15:32:46 +0200

    Spostato DB su porta 27017

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,4 +1,4 @@
-const url = 'mongodb://localhost:27042/pings';
+const url = 'mongodb://localhost:27017/pings';
 // const url = 'mongodb://albertobagnacani:modeling4cloud@10.0.0.14/pings'; // TODO secrets?
 //const url = 'mongodb://albertobagnacani:modeling4cloud@137.204.57.93:27017/pings'; // TODO secrets?
 var mongoose = require('mongoose');

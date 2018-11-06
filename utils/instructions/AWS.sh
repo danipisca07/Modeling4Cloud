1261e39838fc81e17bc4bf08363c896a404eb1d9
danipisca07 <danipisca07@gmail.com>
2018-11-06 16:13:10 +0100

    Aggiunti commenti spiegazione configurazione provider

--- utils/instructions/AWS.sh
+++ utils/instructions/AWS.sh
@@ -1,33 +1,46 @@
+####Settings for hping direct tests
 #Provider, source-ssh-keyfile source-ip destination-ip source-zone destination-zone sequence-number
 pings=(
 		"AWS ./keys/aws-imol-virginia.pem 54.147.226.152 18.130.223.116 us-east-1 eu-west-2c 2" 
 		"AWS ./keys/aws-imol-tokyo.pem 18.179.61.138 18.130.223.116 ap-northeast-1 eu-west-2c 3" 
 		"AWS ./keys/aws-imol-virginia.pem 54.147.226.152 18.179.61.138 ap-northeast-1 us-east-1 4")
 
+
+
+####Settings for iperf direct tests
 #Provider, source-ssh-keyfile source-ip destination-ssh-keyfile destination-ip source-zone destination-zone connection-port sequence-number hour-interval duration parallel
 iperfs=("AWS ./keys/aws-imol-london.pem 18.130.223.116 ./keys/aws-imol-paris.pem 35.180.92.154 eu-west-2c eu-west-3c 80 1 14 1 1" 
 	"AWS ./keys/aws-imol-virginia.pem 54.147.226.152 ./keys/aws-imol-london.pem 18.130.223.116 us-east-1 eu-west-2c 80 2 14 1 1" 
 	"AWS ./keys/aws-imol-tokyo.pem 18.179.61.138 ./keys/aws-imol-london.pem 18.130.223.116 ap-northeast-1 eu-west-2c 80 3 14 1 1" 
 	"AWS ./keys/aws-imol-virginia.pem 54.147.226.152 ./keys/aws-imol-tokyo.pem 18.179.61.138 ap-northeast-1 us-east-1 80 4 14 1 1")
 
+
+
+
+####Settings for qperf direct tests
 #Provider, source-ssh-keyfile source-ip destination-ssh-keyfile destination-ip source-zone destination-zone connection-port sequence-number
 qperfs=("AWS ./keys/aws-imol-paris.pem 35.180.92.154 ./keys/aws-imol-london.pem 18.130.223.116 eu-west-3c eu-west-2c 8080 1")
 
+
+
+
+####Settings for crossregion and sameregion tests
 #Global settings
 PROVIDER=AWS
 
 #Hping settings
-HPINGBASEPORT=6000
-HPING_SECONDS_INTERVAL=10
-HPING_BIDIRECTIONAL=0
+HPINGBASEPORT=6000 #Starting port of the incremental range for hping used ports
+HPING_SECONDS_INTERVAL=10 #Time between tests in seconds
+HPING_BIDIRECTIONAL=0 #Test in both directions between regions(1) or not(0)
 
 #Iperf settings
-PORT=80
-IPERF_HOUR_INTERVAL=25
-DURATION=1
-PARALLEL=1
-IPERF_BIDIRECTIONAL=0
+PORT=80 #Port used for iperf tests
+IPERF_HOUR_INTERVAL=25 #Time between tests in hours
+DURATION=1 #Duration of each bandwitdh test
+PARALLEL=1 #Number of parallel connection opened by each iperf bandwidth test
+IPERF_BIDIRECTIONAL=0 #Test in both directions between regions(1) or not(0)
 
+#Information about vms to use for the cross region tests
 vms=(
 "./keys/aws-imol-virginia.pem 107.21.185.163 us-east-1"
 "./keys/aws-imol-ohio.pem 18.222.248.68 us-east-2"
